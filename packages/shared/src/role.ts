9ccc2ba097c5ce7fe16af35dafe27b371c66cbe2
Kylian <kylian.lec@gmail.com>
2026-06-29 17:41:43 +0200

    chore: add generic type for enum

--- packages/shared/src/role.ts
+++ packages/shared/src/role.ts
@@ -1,6 +1,8 @@
+import type { TypesValuesOf } from "./type/generics.type";
+
 export const Role = {
   COACH: "COACH",
   ATHLETE: "ATHLETE",
 } as const;
 
-export type Role = (typeof Role)[keyof typeof Role];
+export type Role = TypesValuesOf<typeof Role>;
