ad228cbfe3267eb78af69d022ff514467520ad32
Kylian <kylian.lec@gmail.com>
2026-09-02 15:11:23 +0200

    feat(shared): une fabrique de formateurs, instanciee une fois par app refs #137

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -563,6 +563,8 @@ export {
 } from "./util/date-format.util";
 export type { FeedbackMediaSlots, FeedbackReadState } from "./util/feedback.util";
 export { countUnreadFeedbacks, remainingMediaSlots } from "./util/feedback.util";
+export type { Formatters } from "./util/formatter.util";
+export { createFormatters } from "./util/formatter.util";
 export type { InvoiceStateBadge, InvoiceTiming } from "./util/invoice.util";
 export {
   countOverdueInvoices,
